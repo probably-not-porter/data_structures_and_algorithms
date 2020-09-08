30d7c2708d16173b30485e579f0ab4c692a4508d
probably-not-porter <pelibby16@earlham.edu>
2020-09-07 20:19:35 -0400

    insertion sort

---
+++ sort_algorithms/insertion_sort/insertion_sort.py
@@ -0,0 +1,20 @@
+# Insertion Sort
+# Python
+# Porter L
+
+def insertionSort(Arr): 
+    for x in range(1, len(Arr)): 
+        key = Arr[x] 
+        y = x-1
+        while y >= 0 and key < Arr[y] : 
+                Arr[y + 1] = Arr[y] 
+                y -= 1
+        Arr[y + 1] = key 
+    return Arr
+
+# Example
+if __name__=='__main__': 
+    test_array = [2,7,22,71,5,3,11,9,7,5,4,3,56,3,23,43,97,3]
+    print(test_array) # unsorted
+    test_array = insertionSort(test_array)
+    print(test_array) # sorted
